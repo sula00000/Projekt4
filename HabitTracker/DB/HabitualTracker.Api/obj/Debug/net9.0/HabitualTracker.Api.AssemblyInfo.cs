b7beb0c87e509cf31c2a792c71b52d8f4dd6c56b
sulaiman hassan <sulaimanh0052@gmail.com>
2025-11-26 14:51:58 +0100

    ew

--- HabitTracker/DB/HabitualTracker.Api/obj/Debug/net9.0/HabitualTracker.Api.AssemblyInfo.cs
+++ HabitTracker/DB/HabitualTracker.Api/obj/Debug/net9.0/HabitualTracker.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HabitualTracker.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dbcd94bd081c9ef126f6d2d8027070a8cbc5b80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+133eb8058c0673bddb26f2772b2b6d3dacf045ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("HabitualTracker.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HabitualTracker.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
